51db13f88144ebd8ab49778cab910496ff4a4c07
cynwxh <949883887@qq.com>
2019-03-18 10:11:37 +0800

    完成

--- js/common.js
+++ js/common.js
@@ -1,24 +1,23 @@
-
 // 头部导航栏菜单按钮下拉动效
-$("header .menu").on("click",function(){
-    if($(".select").height()==0){
-        $(".select").css("height","80vh")
-        $(".select").css("padding-top","0.8rem")
-    }else{
-        $(".select").css("height","0")
-        $(".select").css("padding-top","0")
+$("header .menu").on("click", function () {
+    if ($(".select").height() == 0) {
+        $(".select").css("height", "80vh")
+        $(".select").css("padding-top", "0.8rem")
+    } else {
+        $(".select").css("height", "0")
+        $(".select").css("padding-top", "0")
     }
 })
-// 二级菜单下拉动销
-$(".select-child").on("click",function(){
-    var listHeight=0;
-    $(this).find(".box ul li").each(function(){
-        listHeight+=$(this).height();
+// 二级菜单下拉
+$(".select-child").on("click", function () {
+    var listHeight = 0;
+    $(this).find(".box ul li").each(function () {
+        listHeight += $(this).height();
     })
-    if($(this).find(".box").height()==0){
-        $(this).find(".box").css("height",listHeight+"px");
-        $(this).siblings().find(".box").css("height","0");
-    }else{
-        $(this).find(".box").css("height","0");
+    if ($(this).find(".box").height() == 0) {
+        $(this).find(".box").css("height", listHeight + "px");
+        $(this).siblings().find(".box").css("height", "0");
+    } else {
+        $(this).find(".box").css("height", "0");
     }
 })
